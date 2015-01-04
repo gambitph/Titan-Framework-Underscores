2774086c82d31f723adebc74a4c49197b0d60a4a
Benjamin Intal <bfintal@gmail.com>
2015-01-04 11:59:12 +0800

    Print our own form instead since it's a shortcode

--- plugin.php
+++ plugin.php
@@ -17,7 +17,43 @@ require_once( 'underscoresme-generator.php' );
 add_shortcode( 'underscores', '_underscores' );
 function _underscores( $atts, $content = null ) {
 	wp_enqueue_style( 'titan-generator', plugins_url( 'styles.css', __FILE__ ), null, '0.1' );
-	do_action( 'underscoresme_print_form' ); 
+	//do_action( 'underscoresme_print_form' ); 
+	
+	return '
+		<div id="generator-form" class="generator-form-skinny">
+			<form method="POST">
+				<input type="hidden" name="underscoresme_generate" value="1" />
+
+				<section class="generator-form-inputs">
+					<section class="generator-form-primary">
+						<label for="underscoresme-name">Theme Name</label>
+						<input type="text" id="underscoresme-name" name="underscoresme_name" placeholder="Theme Name" />
+					</section><!-- .generator-form-primary -->
+
+					<section class="generator-form-secondary">
+						<label for="underscoresme-slug">Theme Slug</label>
+						<input type="text" id="underscoresme-slug" name="underscoresme_slug" placeholder="Theme Slug" />
+
+						<label for="underscoresme-author">Author</label>
+						<input type="text" id="underscoresme-author" name="underscoresme_author" placeholder="Author" />
+
+						<label for="underscoresme-author-uri">Author URI</label>
+						<input type="text" id="underscoresme-author-uri" name="underscoresme_author_uri" placeholder="Author URI" />
+
+						<label for="underscoresme-description">Description</label>
+						<input type="text" id="underscoresme-description" name="underscoresme_description" placeholder="Description" />
+
+						<input type="checkbox" id="underscoresme-sass" name="underscoresme_sass" value="1">
+						<label for="underscoresme-sass">_sassify!</label>
+					</section><!-- .generator-form-secondary -->
+				</section><!-- .generator-form-inputs -->
+
+				<div class="generator-form-submit">
+					<input type="submit" name="underscoresme_generate_submit" value="Generate" />
+					<span class="generator-form-version">Based on <a href="http://github.com/automattic/_s">_s from github</a></span>
+				</div><!-- .generator-form-submit -->
+			</form>
+		</div><!-- .generator-form -->';
 }
 
 
